01c8b5213abc7b8b644763020441fe589e7b6cec
Bertmoss <b3rtm0ss@gmail.com>
2022-09-08 08:57:40 +0200

    Add hide and display modal button

--- src/main/main.js
+++ src/main/main.js
@@ -1,11 +1,18 @@
 import {modal} from "./modal/modal";
 import {display} from "./display/display"
-
+import { noteButton } from "./modal/__side/modal__side";
 
 const mainDiv = document.createElement("main");
 mainDiv.classList.add("main");
 
+const addBtn = document.createElement("button");
+addBtn.setAttribute("type", "button");
+addBtn.textContent = "+"
+addBtn.addEventListener("click", () => {
+  modal.classList.remove("main__modal--hidden")
+})
 
+mainDiv.appendChild(addBtn);
 mainDiv.appendChild(modal);
 mainDiv.appendChild(display);
 
--- src/main/modal/modal.js
+++ src/main/modal/modal.js
@@ -6,14 +6,29 @@ import {noteForm} from "./__form/modal__form--note"
 let modal = document.createElement("div")
 modal.classList.add("modal", "main__modal");
 
+let closeBtn = document.createElement("button");
+closeBtn.setAttribute("type", "button");
+closeBtn.setAttribute("style", "position:absolute") /* has to be moved to scss!!! */
+closeBtn.textContent = "x";
+modal.appendChild(closeBtn);
 
+function hideModal() {
+  removeForm();
+  formContainer.appendChild(taskForm);
+  modal.classList.add("main__modal--hidden");
+} 
+closeBtn.addEventListener("click", hideModal)
+
+/* side */
 modal.appendChild(side.sideDiv);
 
+/* form on initial load */
 let formContainer = document.createElement("div");
 formContainer.appendChild(taskForm);
 formContainer.classList.add("modal__form")
 modal.appendChild(formContainer);
 
+/* remove form func */
 function removeForm() {
   while (formContainer.firstChild) {
     formContainer.firstChild.reset()
@@ -21,6 +36,7 @@ function removeForm() {
   }
 }
 
+/* side button event listeners */
 side.taskButton.addEventListener("click", () =>{
   removeForm();
   formContainer.appendChild(taskForm);
